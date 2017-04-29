792330c92f1f31060695d9a1ae5cfd1f09ccb6ec
Türhan Yıldırım <yildirimturhan@gmail.com>
2017-04-29 17:03:36 +0300

    Webapi ninject ve business katmanı entegrasyonu eklendi.

---
+++ Abc.Framework/Abc.Northwind.WebApi/App_Start/NinjectWebCommon.cs
@@ -0,0 +1,75 @@
+using System.Web.Http;
+using Abc.Northwind.Business.DependencyResolvers.Ninject;
+using WebApiContrib.IoC.Ninject;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(Abc.Northwind.WebApi.App_Start.NinjectWebCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(Abc.Northwind.WebApi.App_Start.NinjectWebCommon), "Stop")]
+
+namespace Abc.Northwind.WebApi.App_Start
+{
+    using System;
+    using System.Web;
+
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+    using Ninject;
+    using Ninject.Web.Common;
+
+    public static class NinjectWebCommon 
+    {
+        private static readonly Bootstrapper bootstrapper = new Bootstrapper();
+
+        /// <summary>
+        /// Starts the application
+        /// </summary>
+        public static void Start() 
+        {
+            DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
+            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
+            bootstrapper.Initialize(CreateKernel);
+        }
+        
+        /// <summary>
+        /// Stops the application.
+        /// </summary>
+        public static void Stop()
+        {
+            bootstrapper.ShutDown();
+        }
+        
+        /// <summary>
+        /// Creates the kernel that will manage your application.
+        /// </summary>
+        /// <returns>The created kernel.</returns>
+        private static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            try
+            {
+                kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+                kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+
+                //TODO:Ninject ile resolve et
+                GlobalConfiguration.Configuration.DependencyResolver = new NinjectResolver(kernel);
+
+                RegisterServices(kernel);
+                return kernel;
+            }
+            catch
+            {
+                kernel.Dispose();
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Load your modules or register your services here!
+        /// </summary>
+        /// <param name="kernel">The kernel.</param>
+        private static void RegisterServices(IKernel kernel)
+        {
+            //TODO: ninject module set edilmesi
+            kernel.Load(new BusinessModule());
+        }        
+    }
+}
---
+++ Abc.Framework/Abc.Northwind.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Abc.Northwind.Business.Abstract;
+using Abc.Northwind.Entities.Concrete;
+
+namespace Abc.Northwind.WebApi.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public HttpResponseMessage Get()
+        {
+            var result = _productService.GetAll()
+                .Select(p => new Product()
+                 {
+                     CategoryId = p.CategoryId,
+                     ProductId = p.ProductId,
+                     ProductName = p.ProductName,
+                     UnitPrice = p.UnitPrice,
+                     UnitsInStock = p.UnitsInStock
+                 }).ToList();
+
+            HttpResponseMessage message = Request.CreateResponse(HttpStatusCode.OK, result);
+            return message;
+        }
+    }
+}
